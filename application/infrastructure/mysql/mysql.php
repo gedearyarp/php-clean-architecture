ae388fad128d1bcb70b13226d25d3d69dc8975b4
GedeAryaRP <71829426+gedearyarp@users.noreply.github.com>
2022-09-16 09:01:17 +0700

    delete unused comment

--- application/infrastructure/mysql/mysql.php
+++ application/infrastructure/mysql/mysql.php
@@ -1,20 +1,16 @@
 <?php
 
-// create class for mysql
 class MySql {
     private $connection;
 
-    // constructor
     public function __construct($host, $user, $pass, $db) {
         $this->connection = new mysqli($host, $user, $pass, $db);
     }
 
-    // destructor
     public function __destruct() {
         $this->connection->close();
     }
 
-    // query
     public function query($sql) {
         return $this->connection->query($sql);
     }
